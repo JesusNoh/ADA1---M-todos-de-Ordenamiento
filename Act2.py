38ed94e4375bb9ddf25d24b386ec4de795bba4da
Jesus Noh <jesusnoh62@gmail.com>
2024-11-05 12:53:16 -0600

    Act2.py

--- Act2.py
+++ Act2.py
@@ -7,7 +7,6 @@ def insercion(arr):
             j -= 1
         arr[j + 1] = clave
 
-# Lista desordenada
 lista_insercion = [12, 11, 13, 5, 6]
 insercion(lista_insercion)
 print("Lista ordenada con inserción:", lista_insercion)
